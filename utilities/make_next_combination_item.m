526be2889c6073bd22f4cd02c21ba0050e7fc3ae
coppolam <m.coppola@tudelft.nl>
2017-11-30 20:29:38 +0100

    fixed make_next_combination_item

--- utilities/make_next_combination_item.m
+++ utilities/make_next_combination_item.m
@@ -1,8 +1,12 @@
-function c_next = make_next_combination_item(c_item, n_max)
+function c_next = make_next_combination_item(c_item, n_max, forced_idx)
 
+% Index to change
 i = find(c_item<=n_max,1,'last');
-if c_item(i)+1 > 4
+if c_item(i)+1 > n_max || forced_idx~=0 % Reset row
     i = find(c_item<n_max,1,'last');
+    if forced_idx~=0 && c_item(i)+1 <= n_max
+        i = forced_idx;
+    end
     c_item(i) = c_item(i)+1;
     c_item(i+1:end) = c_item(i);
 else
