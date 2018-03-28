35bd2772f19f39731fbfb887d18dd25d9e8dec68
coppolam <m.coppola@tudelft.nl>
2018-03-28 12:12:01 +0200

    changed rand_from_vector to use weighted probabilities

--- utilities/vector_modifications/rand_from_vector.m
+++ utilities/vector_modifications/rand_from_vector.m
@@ -1,14 +1,19 @@
-function [selected] = rand_from_vector(vector, n_elements)
+function [selected] = rand_from_vector(vector, n_elements, probabilities)
 
 if nargin < 2
     n_elements = 1;
 end
 
-if length(vector) > 1
-    selected = randsample(vector, n_elements);
+if nargin == 3 && sum(probabilities)==1
+    p = cumsum([0; probabilities(1:end-1).'; 1+1e3*eps]);
+    [~,a] = histc(rand,p);
+    selected = vector(a);
 else
-    selected = vector;
+    if length(vector) > 1
+        selected = randsample(vector, n_elements);
+    else
+        selected = vector;
+    end
 end
-
 end
 
