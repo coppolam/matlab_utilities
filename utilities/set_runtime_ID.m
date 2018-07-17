6a664d5785661fe83cefcde80e05e3ff31d1dfed
coppolam <m.coppola@tudelft.nl>
2018-07-17 17:35:49 +0200

    modified with input

--- utilities/set_runtime_ID.m
+++ utilities/set_runtime_ID.m
@@ -1,7 +1,13 @@
 function [runtimeID] = set_runtime_ID()
 
 rng shuffle
-runtimeID  = randi(10000);
-fprintf('\nMy runtime ID is %d',runtimeID);
+
+prompt    = '\n\nDo you desire a runtime ID? \n(Click <Enter> for a random one) \n  >> ';
+runtimeID = str2double(input(prompt,'s'));
+if isempty(runtimeID)
+    runtimeID  = randi(10000);
+    fprintf('\nMy random runtime ID is %d',runtimeID);
+end
+
 
 end
