110c9f35b1bb15994cf18677b5d03d96a5fd1435
coppolam <m.coppola@tudelft.nl>
2018-11-25 16:48:47 +0100

    mod version of pagerank

---
+++ math/graphtheory/pagerank_mod.m
@@ -0,0 +1,71 @@
+function [pr,i] = pagerank_mod(H,alpha,v,D,E)
+% PageRank computes the PageRank vector for an n-by-n Markov
+% matrix H with starting vector pi0 (a row vector),
+% scaling parameter alpha (scalar), and teleportation
+% vector v (a row vector). Uses power method.
+%
+% EXAMPLE:[pr,iterations]=pagerank(H,alpha,1e-8,v);
+%
+% INPUT:
+%   H     = row-normalized hyperlink matrix (n-by-n sparse matrix)
+%   alpha = follow probability, scalar scaling parameter in PageRank model (scalar), 
+%   v     = teleportation vector (1-by-n row vector)
+%   D     = Dangling node matrix
+%   E     = Teleportation matrix (can be defined instead of v)
+%
+% OUTPUT:
+%   pr = PageRank vector
+%   i  = Number of iterations
+%
+% Adapted from Langville et al., Google's PageRank and beyond, 2006
+%
+% Mario Coppola, 2018
+
+% The starting vector is usually set to the uniform vector,
+n = size(H,1);
+pr_0 = 1/n * ones(1,n);
+
+% Make sure H is normalized
+H = H./sum(H,2);
+H(isnan(H)) = 0;
+
+% Personalization vector default
+if nargin < 3
+    v = 1/n * ones(n,1);
+    % Get "a" vector, where a(i)=1, if row i is dangling node and 0
+    rowsumvector = ones(1,n)*H';
+    nonzerorows  = find(rowsumvector);
+    zerorows     = setdiff(1:n,nonzerorows);
+    l            = length(zerorows);
+    a            = sparse(zerorows,ones(l,1),ones(l,1),n,1);
+end
+
+% Iterative procedure
+i = 0;        % Counter
+pr = pr_0;    % Pagerank
+residual = 1; % Residual (initialize high, doesn't really matter)
+tol = 1e-8;   % Convergence tolerance (scalar, e.g. 1e-8)
+while residual >= tol
+    i = i+1;
+    pr_previous = pr;
+    % The following two are equivalent. One is the one from the book
+    % the other one is just organized such that the random and follow items
+    % are more clearly separated into terms 1 and 2.
+    
+    % Simplified expression from book
+    % pr = alpha * pr * H + ( alpha * (pr*a) + 1 - alpha) * v';
+    
+    % Divide two terms
+    if nargin <= 3
+        D  = a*v';        % Dangling node teleportation matrix
+        E  = ones(1,n)*v; % Teleportation matrix
+    end
+    % Calculate the Pagerank, pr = pr * G
+    G = alpha * (H + D) + (eye(size(alpha)) - alpha) *  E;
+%     G = G./sum(G,2);
+%     G(isnan(G)) = 0;
+    pr = pr * G; 
+    residual = norm ( pr - pr_previous, 1 );
+end
+
+end
